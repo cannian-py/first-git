b5d2deeb469a32851018eb041b3441c25a157d88
cannian <879142964@qq.com>
2020-07-20 23:54:49 +0800

    java_text

---
+++ java_text/src/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!!");
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
